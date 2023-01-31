e57019cda86d2f98c9f259478c7982658240a7a7
Ulisesplapler <ulisesp@lcg.unam.mx>
2023-01-31 14:02:12 -0600

    Agregar notas discusion

---
+++ R/03-BiocProgram_discussion.R
@@ -0,0 +1,28 @@
+## Nombre: Epimix
+#  Autores:
+#  Citas:
+## sitio web :  https://bioconductor.org/packages/release/bioc/html/EpiMix.html
+#
+
+# Descripcion: descarga y analisis de datos de DNA metilado y datos
+# de expresion, permite una visualizacion interactiva y una funcional
+# anotacion.
+
+# Observaciones: El vignette esta muy bien descrito, con ejemplos de usos
+# y resultados (graficas) que se pueden obtener mediante su uso.
+# No estan pasando las pruebas en los SO Windows o Mac,
+# tampoco se han hecho preguntas sobre el paquete.
+# Hay disponible un repositorio en github con el codigo y ejemplos.
+
+
+## Nombre: crisprDesign
+
+## sitio web : https://github.com/crisprVerse/crisprDesign
+
+# Descripcion: Suite para el disenio y anotacion de gRNAs, que puede emplearse
+# para cualquier organismo
+
+# Observaciones: No tiene reportes de error, tampoco se han hecho preguntas
+# a los autores. EL viggnette esta muy explicito, incluso incluye terminologia
+# biologica, asi como un ejemplo de uso del paquete.
+# Tiene un repositorio en github muy activo
